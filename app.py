7cee6d63ab5ef14879626f914c62dfceb1195934
Abhinav2k4 <abhinavsuryak@gmail.com>
2025-06-09 22:07:50 +0530

    Fixed Flask host/port for Railway

--- app.py
+++ app.py
@@ -2,6 +2,7 @@ from flask import Flask, request, jsonify
 import numpy as np
 from PIL import Image
 import io
+import os
 import tensorflow as tf
 
 
@@ -47,4 +48,5 @@ def predict():
     })
 
 if __name__ == '__main__':
-    app.run(debug=True)
+    port = int(os.environ.get("PORT", 5000))
+    app.run(host='0.0.0.0', port=port)
